f6777ec624b8daf469b1a0747bbe41fa1a10ded0
Kraken57 <kasaad1609@gmail.com>
2024-02-02 20:27:04 +0530

    firebase rules added

--- functions/index.js
+++ functions/index.js
@@ -1,18 +1,19 @@
-const { initializeApp } = require("firebase-admin/app")
-const { getFirestore, FieldValue } = require("firebase-admin/firestore")
-const {onDocumentCreated, } = require("firebase-functions/v2/firestore")
-initializeApp()
+/**
+ * Import function triggers from their respective submodules:
+ *
+ * const {onCall} = require("firebase-functions/v2/https");
+ * const {onDocumentWritten} = require("firebase-functions/v2/firestore");
+ *
+ * See a full list of supported triggers at https://firebase.google.com/docs/functions
+ */
 
-exports.createBoardData = onDocumentCreated('users/{uid}/boards/{boardId}', async event => {
-    const {uid, boardId} = event.params
-    const firestore = getFirestore()
+const {onRequest} = require("firebase-functions/v2/https");
+const logger = require("firebase-functions/logger");
 
-    return await firestore.doc(`users/${uid}/boardsData/${boardId}`).set({
-        tabs:{
-            todos: [],
-            inProgress: [],
-            completed: []
-        },
-        lastUpdated: FieldValue.serverTimestamp(),
-    })
-})
+// Create and deploy your first functions
+// https://firebase.google.com/docs/functions/get-started
+
+// exports.helloWorld = onRequest((request, response) => {
+//   logger.info("Hello logs!", {structuredData: true});
+//   response.send("Hello from Firebase!");
+// });
